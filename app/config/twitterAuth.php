34d99654b86088b3232b6bceabe1f59ba5d6900d
frankito9999 <perfumefrankito@hotmail.com>
2015-05-14 23:18:51 -0400

    nuevo login social

--- app/config/twitterAuth.php
+++ app/config/twitterAuth.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    "base_url" => "http://b2.com/twitterAuth/auth", //write your own url here
+    "base_url" => "http://104.236.203.200/twitterAuth/auth", //write your own url here
     "providers" => array (
         "Twitter" => array (
             "enabled" => true,
